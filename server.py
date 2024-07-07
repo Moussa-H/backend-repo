3ede87361ea961592e24931ad7c0cdbd2d99fac1
Moussa_Haidar <moussahaidar999@gmail.com>
2024-07-07 16:12:54 +0300

    Updated server.py, adding check_run_localhost method

--- server.py
+++ server.py
@@ -3,13 +3,18 @@
 import yaml
 
 class Server:
-    def __init__(self, config_file):
+    def __init__(self, config_file='../config.yaml'):
         with open(config_file, 'r') as file:
             self.config = yaml.safe_load(file)
     
     def print_server_ip(self):
         print(f"Server IP Address: {self.config['ServerIPAddress']}")
 
+    def check_run_localhost(self):
+        if self.config.get('RunLocalhost', False):
+            print("Running on localhost. Please update configurations if necessary.")
+
 if __name__ == "__main__":
-    server = Server('../config.yaml')
+    server = Server()
     server.print_server_ip()
+    server.check_run_localhost()
